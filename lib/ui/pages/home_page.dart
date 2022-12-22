7c43e3ed544fe1d1b4121a06c2bdd7eca18dc5e4
Evolutemaker <adilhanigilikov@gmail.com>
2022-12-23 01:28:32 +0600

    final commit

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -6,6 +6,7 @@ import 'package:leadsdoit/constants.dart';
 import 'package:leadsdoit/data/repositories/game_repo.dart';
 import 'package:leadsdoit/data/models/game.dart';
 import 'package:leadsdoit/ui/widgets/custom_rectangle.dart';
+import 'package:quickalert/quickalert.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({super.key});
@@ -31,70 +32,81 @@ class _HomePageState extends State<HomePage> {
     late InAppWebViewController webView;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: state.when(
-              loading: () {
-                return Container(
-                  width: width,
-                  height: height,
-                  child: Center(
-                      child: Image.asset('assets/images/bigIcon.png')),
-                );
-              },
-              webview: () {
-                return Stack(
-                  children: [
-                    InAppWebView(
-                      initialUrlRequest: URLRequest(
-                        url: Uri.parse(sportWeb),
-                      ),
-                      onWebViewCreated: (InAppWebViewController controller) {
-                        webView = controller;
-                      },
-                      onProgressChanged: (InAppWebViewController controller,
-                          int progress) {},
-                    ),
-                  ],
-                );
-              },
-              loaded: (gameLoaded) {
-                _currentResults = gameLoaded.events;
-                return _currentResults.isNotEmpty
-                    ? Scaffold(
-                        appBar: AppBar(
-                          title: Image.asset(
-                            'assets/images/icon.png',
-                            width: height / (896 / 47),
-                            height: height / (896 / 47),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        body: _gamesListView(_currentResults))
-                    : const SizedBox();
-              },
-              error: () => const Text('error'),
+      appBar: state.maybeMap(
+        orElse: () => null,
+        loaded: (_) {
+          return AppBar(
+            title: Image.asset(
+              'assets/images/icon.png',
+              width: height / (896 / 47),
+              height: height / (896 / 47),
+              fit: BoxFit.cover,
             ),
-          ),
-        ],
+          );
+        },
       ),
+      body: state.when(loading: () {
+        return SizedBox(
+          width: width,
+          height: height,
+          child: Center(
+            child: Image.asset('assets/images/bigIcon.png'),
+          ),
+        );
+      }, webview: () {
+        return SafeArea(
+          child: InAppWebView(
+            initialUrlRequest: URLRequest(
+              url: Uri.parse(sportWeb),
+            ),
+            onWebViewCreated: (InAppWebViewController controller) {
+              webView = controller;
+            },
+            onProgressChanged:
+                (InAppWebViewController controller, int progress) {},
+          ),
+        );
+      }, loaded: (gameLoaded) {
+        _currentResults = gameLoaded.events;
+        return _currentResults.isNotEmpty
+            ? _gamesListView(currentResults: _currentResults)
+            : const SizedBox();
+      }, error: () {
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          QuickAlert.show(
+            context: context,
+            type: QuickAlertType.error,
+            text: 'Internet connection was lost',
+          );
+        });
+      }),
     );
   }
 }
 
-Widget _gamesListView(List<Events> currentEvents) {
-  return ListView.separated(
-    padding: const EdgeInsets.only(top: 24),
-    separatorBuilder: (_, index) => const SizedBox(height: 16.0),
-    shrinkWrap: true,
-    itemCount: currentEvents.length,
-    itemBuilder: (context, index) {
-      final event = currentEvents[index];
-      return Padding(
-        padding: const EdgeInsets.only(right: 20, left: 20),
-        child: CustomRectangle(event: event),
-      );
-    },
-  );
+class _gamesListView extends StatelessWidget {
+  const _gamesListView({
+    Key? key,
+    required List<Events> currentResults,
+  })  : _currentResults = currentResults,
+        super(key: key);
+
+  final List<Events> _currentResults;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      padding: const EdgeInsets.only(top: 24),
+      separatorBuilder: (_, index) => const SizedBox(height: 16.0),
+      shrinkWrap: true,
+      itemCount: _currentResults.length,
+      itemBuilder: (context, index) {
+        final event = _currentResults[index];
+        return Padding(
+          padding: const EdgeInsets.only(right: 20, left: 20),
+          child: CustomRectangle(event: event),
+        );
+      },
+    );
+  }
 }
